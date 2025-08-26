49a5f0981d2aa583d3313d8bf18f8aa924e65771
jianzhichun <zzchun12826@gmail.com>
2025-08-26 17:46:32 +0800

    refactor(pkg): lazy-load gradio_ui to avoid import-time side effects

--- spatial_fabric_sdk/__init__.py
+++ spatial_fabric_sdk/__init__.py
@@ -8,7 +8,10 @@ from .client import SpatialFabricClient
 from .models import SpatialData, SpatialMetadata, SearchFilter, SearchResult
 from .exceptions import SpatialFabricError, HandleError, GardError, ConfigurationError
 from .config import ClientConfig
-from .gradio_ui import gradio_ui
+def gradio_ui() -> None:
+    """Lazy import UI to avoid heavy side effects at package import time."""
+    from .gradio_ui import gradio_ui as _gradio_ui
+    _gradio_ui()
 
 __version__ = "1.0.0"
 __author__ = "Spatial Fabric Team"
